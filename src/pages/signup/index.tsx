053de08a38a9543b21f3dd9ca954d104a1983443
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-15 17:05:38 +0200

    Finished signup page layout

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,64 @@
+import s from "../../styles/signup.module.scss";
+import Header from "../../components/Header";
+import { FC } from "react";
+import { Row, Col, Input, Button, Form } from "antd";
+
+const Signup: FC = () => {
+  return (
+    <>
+      <Header />
+      <Row justify="center" className={s.row}>
+        <Col xl={20} md={22} xs={24}>
+          <Row justify="center">
+            <Col lg={14} md={18} sm={20} xs={24}>
+              <div className={s.paper}>
+                <Form className={s.form}>
+                  <Form.Item
+                    name="username"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your username!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Name" />
+                  </Form.Item>
+                  <Form.Item
+                    name="email"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your email!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Email" type="email" />
+                  </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                  >
+                    <Input.Password placeholder="Password" />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                      Sign up
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Signup;
