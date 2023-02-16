5891bf867963e17c4ccc9a0e7f79fbe0cbcc118e
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-16 12:39:47 +0200

    firebase keys store in .env file

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnDb94Suaf1kwO_YC9RnAt17tWAnpwyg0",
-  authDomain: "phonebook-93005.firebaseapp.com",
-  projectId: "phonebook-93005",
-  storageBucket: "phonebook-93005.appspot.com",
-  messagingSenderId: "531817853691",
-  appId: "1:531817853691:web:2fa6fe1265507a7c55365e",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
