a20c629c863e03dcf07c885e95341faf04f2b76c
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-16 15:50:56 +0200

    Added image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import s from "../styles/index.module.scss";
 import Header from "../components/Header";
 import { Typography, Button, Row, Col } from "antd";
+import Image from "next/image";
+import people from "../assets/images/people.png";
 
 const { Title, Paragraph } = Typography;
 
@@ -11,16 +13,27 @@ const Home = () => {
       <Row justify="center">
         <Col xl={20} md={22} xs={24}>
           <main className={s.main}>
-            <Title level={1}>Welcome to Phonebook App</Title>
-            <Paragraph>
-              Are you looking for a free contact management tool that’ll help
-              you become more productive and speed up the communication process?
-              If yes, then Phonebook is the app for you. It lets you store all
-              your contacts under a single umbrella.
-            </Paragraph>
-            <Button type="primary" size="large">
-              Get Started
-            </Button>
+            <div className={s.col}>
+              <Title level={1}>Welcome to Phonebook App</Title>
+              <Paragraph>
+                Are you looking for a free contact management tool that’ll help
+                you become more productive and speed up the communication
+                process? If yes, then Phonebook is the app for you. It lets you
+                store all your contacts under a single umbrella.
+              </Paragraph>
+              <Button type="primary" size="large">
+                Get Started
+              </Button>
+            </div>
+            <div className={s.col}>
+              <Image
+                src={people}
+                alt="People with phones"
+                width={540}
+                height={540}
+                quality={100}
+              />
+            </div>
           </main>
         </Col>
       </Row>
