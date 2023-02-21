a34b904f329eeb968690657128118c633176b2f4
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-21 18:14:45 +0200

    Changed item from LS for req interceptor

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,12 +3,14 @@ import axios from "axios";
 export const BASE_URL = "https://ec00-178-212-242-26.eu.ngrok.io";
 
 const api = axios.create({
-  // withCredentials: true, ?
+  // withCredentials: true,
   baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    "accessToken"
+  )}`;
   return config;
 });
 
