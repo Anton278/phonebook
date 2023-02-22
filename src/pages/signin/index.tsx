cc475b67620f74a7ede4971b1558def40fec0690
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-22 15:18:19 +0200

    Applied Container component to all pages

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -2,7 +2,7 @@ import s from "../../styles/signin.module.scss";
 import Header from "../../components/Header";
 import { Paper } from "../../components/Paper";
 import { FC } from "react";
-import { Row, Col, Input, Button, Form, Typography } from "antd";
+import { Input, Button, Form, Typography } from "antd";
 import { useRouter } from "next/router";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { useAppSelector } from "../../hooks/useAppSelector";
@@ -13,6 +13,7 @@ import {
 import { signin } from "../../redux/auth/thunks";
 import { SigninValues } from "../../types/SigninValues";
 import { withOnlyPublic } from "@/hocs/withOnlyPublic";
+import { Container } from "@/components/Container";
 
 const Signin: FC = () => {
   const router = useRouter();
@@ -30,54 +31,42 @@ const Signin: FC = () => {
   return (
     <>
       <Header />
-      <Row justify="center" className={s.row}>
-        <Col xl={20} md={22} xs={24}>
-          <Row justify="center">
-            <Col lg={14} md={18} sm={20} xs={24}>
-              <Paper>
-                <Form className={s.form} onFinish={handleSignin}>
-                  <Form.Item
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your email!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="Email" type="email" />
-                  </Form.Item>
-                  <Form.Item
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your password!",
-                      },
-                    ]}
-                  >
-                    <Input.Password placeholder="Password" />
-                  </Form.Item>
-                  {error && (
-                    <Typography.Paragraph type="danger">
-                      {error}
-                    </Typography.Paragraph>
-                  )}
-                  <Form.Item>
-                    <Button
-                      type="primary"
-                      htmlType="submit"
-                      loading={isProcessing}
-                    >
-                      Sign In
-                    </Button>
-                  </Form.Item>
-                </Form>
-              </Paper>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <Container justifyCenter>
+        <Paper>
+          <Form className={s.form} onFinish={handleSignin}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+            >
+              <Input placeholder="Email" type="email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+            {error && (
+              <Typography.Paragraph type="danger">{error}</Typography.Paragraph>
+            )}
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={isProcessing}>
+                Sign In
+              </Button>
+            </Form.Item>
+          </Form>
+        </Paper>
+      </Container>
     </>
   );
 };
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,7 @@
 import s from "../../styles/signup.module.scss";
 import Header from "../../components/Header";
 import { FC, useContext } from "react";
-import { Row, Col, Input, Button, Form, Typography } from "antd";
+import { Input, Button, Form, Typography } from "antd";
 import { Paper } from "../../components/Paper";
 import { useRouter } from "next/router";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
@@ -14,6 +14,7 @@ import {
 } from "../../redux/auth/selectors";
 import { MessagesContext } from "../_app";
 import { withOnlyPublic } from "@/hocs/withOnlyPublic";
+import { Container } from "@/components/Container";
 
 const Signup: FC = () => {
   const messageApi = useContext(MessagesContext);
@@ -33,65 +34,53 @@ const Signup: FC = () => {
   return (
     <>
       <Header />
-      <Row justify="center" className={s.row}>
-        <Col xl={20} md={22} xs={24}>
-          <Row justify="center">
-            <Col lg={14} md={18} sm={20} xs={24}>
-              <Paper>
-                <Form className={s.form} onFinish={handleSignup}>
-                  <Form.Item
-                    name="username"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your username!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="Name" />
-                  </Form.Item>
-                  <Form.Item
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your email!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="Email" type="email" />
-                  </Form.Item>
-                  <Form.Item
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your password!",
-                      },
-                    ]}
-                  >
-                    <Input.Password placeholder="Password" />
-                  </Form.Item>
-                  {error && (
-                    <Typography.Paragraph type="danger">
-                      {error}
-                    </Typography.Paragraph>
-                  )}
-                  <Form.Item>
-                    <Button
-                      type="primary"
-                      htmlType="submit"
-                      loading={isProcessing}
-                    >
-                      Sign Up
-                    </Button>
-                  </Form.Item>
-                </Form>
-              </Paper>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <Container justifyCenter>
+        <Paper>
+          <Form className={s.form} onFinish={handleSignup}>
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input placeholder="Name" />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+            >
+              <Input placeholder="Email" type="email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+            {error && (
+              <Typography.Paragraph type="danger">{error}</Typography.Paragraph>
+            )}
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={isProcessing}>
+                Sign Up
+              </Button>
+            </Form.Item>
+          </Form>
+        </Paper>
+      </Container>
     </>
   );
 };
