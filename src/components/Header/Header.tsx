1fe24221534c314eac2ed92e2bd20e6de4531e00
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-22 12:14:39 +0200

    Added Add contact button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -32,15 +32,30 @@ const Header = () => {
               </Button>
             </Link>
             {auth && (
-              <Link href="/contacts">
-                <Button
-                  type={router.pathname === "/contacts" ? "primary" : "default"}
-                  size="large"
-                  icon={<ContactsOutlined />}
-                >
-                  <span className={s.buttonText}>Contacts</span>
-                </Button>
-              </Link>
+              <>
+                <Link href="/contacts">
+                  <Button
+                    type={
+                      router.pathname === "/contacts" ? "primary" : "default"
+                    }
+                    size="large"
+                    icon={<ContactsOutlined />}
+                  >
+                    <span className={s.buttonText}>Contacts</span>
+                  </Button>
+                </Link>
+                <Link href="/add-contact">
+                  <Button
+                    type={
+                      router.pathname === "/add-contact" ? "primary" : "default"
+                    }
+                    size="large"
+                    icon={<UserAddOutlined />}
+                  >
+                    <span className={s.buttonText}>Add contact</span>
+                  </Button>
+                </Link>
+              </>
             )}
           </div>
           <div className={s.buttons}>
