e8087a0cadd2edc0dfa7e897b4f86a57a626536f
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-22 12:02:47 +0200

    Take isAuth from store

--- src/hocs/withOnlyPublic.tsx
+++ src/hocs/withOnlyPublic.tsx
@@ -1,19 +1,21 @@
 import { FullscreenSpin } from "../components/FullscreenSpin";
 import { useRouter } from "next/router";
 import { FC, useEffect } from "react";
+import { useAppSelector } from "@/hooks/useAppSelector";
+import { selectIsAuth } from "@/redux/auth/selectors";
 
 export const withOnlyPublic = (Component: FC) => {
   const OnlyPublic = (props: any) => {
-    const auth = false;
+    const isAuth = useAppSelector(selectIsAuth);
     const router = useRouter();
 
     useEffect(() => {
-      if (auth) {
+      if (isAuth) {
         router.push("/");
       }
     }, []);
 
-    if (auth) {
+    if (isAuth) {
       return <FullscreenSpin />;
     }
 
