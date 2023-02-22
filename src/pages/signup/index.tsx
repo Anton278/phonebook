646fdb5f31c398e1041a766ee60e1ccd3665cc62
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-02-22 18:50:42 +0200

    Fixed validation message

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -42,7 +42,7 @@ const Signup: FC = () => {
               rules={[
                 {
                   required: true,
-                  message: "Please input your username!",
+                  message: "Please input your name!",
                 },
               ]}
             >
