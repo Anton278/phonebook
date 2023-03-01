a2795e024869272cb397fcf205fe27162df85024
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-03-01 15:08:27 +0200

    Init express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("server works");
+import express from "express";
+
+const PORT = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json("server works");
+});
+
+app.listen(PORT, () => console.log("Server started on port " + PORT));
