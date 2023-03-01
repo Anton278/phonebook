fc5795ea6dba51f300817fc43ddcf51bff9d1032
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-03-01 17:44:38 +0200

    Added test post endpoint

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ const PORT = 5000;
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/test-post", (req, res) => {
+  console.log("req query ===> ", req.query);
+  console.log("req.body ===> ", req.body);
+  res.status(200).json("post method works!");
+});
+
 app.get("/", (req, res) => {
   res.status(200).json("server works");
 });
