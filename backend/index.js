58ad8c1c1f040c0c32e2a23ad2e2c335e1fd2c01
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-03-02 14:43:33 +0200

    Connected db

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 5000;
+const DB_URL =
+  "mongodb+srv://anton278:marlboroRed278!@cluster0.egjnvtd.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/test-post", (req, res) => {
-  console.log("req query ===> ", req.query);
-  console.log("req.body ===> ", req.body);
-  res.status(200).json("post method works!");
-});
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log("Server started on port " + PORT));
+  } catch (e) {}
+}
 
-app.get("/", (req, res) => {
-  res.status(200).json("server works");
-});
-
-app.listen(PORT, () => console.log("Server started on port " + PORT));
+startApp();
