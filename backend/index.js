d422f1dd819e36166f3f887811b7fb16a6fdc36b
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-03-02 18:13:56 +0200

    Connected cookie-parser and cors libs

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import mongoose from "mongoose";
 import authRouter from "./routers/authRouter.js";
 
@@ -9,6 +11,8 @@ const PORT = 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 app.use("/auth", authRouter);
 
 async function startApp() {
