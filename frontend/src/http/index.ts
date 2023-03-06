7878f8546e9395dc7d72f1d3b02427ed1f82bc83
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-03-06 15:21:14 +0200

    Handled 401 error

--- frontend/src/http/index.ts
+++ frontend/src/http/index.ts
@@ -1,3 +1,4 @@
+import { AuthResponse } from "@/types/AuthResponse";
 import axios from "axios";
 
 export const BASE_URL = "http://localhost:5000";
@@ -11,5 +12,32 @@ api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get<AuthResponse>(
+          BASE_URL + "/auth/refresh",
+          {
+            withCredentials: true,
+          }
+        );
+        localStorage.setItem("token", response.data.accessToken);
+        return api.request(originalRequest);
+      } catch (e) {}
+    }
+    Promise.reject(error);
+  }
+);
 
 export default api;
